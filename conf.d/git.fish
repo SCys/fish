90d3bc5f9d01685a3d9edbc41bb44f7ec547e9e1
SCys <me@iscys.com>
2021-11-01 16:40:14 +0800

    * update git alias

--- conf.d/git.fish
+++ conf.d/git.fish
@@ -2,6 +2,7 @@ alias gs 'git status -s'
 alias gl 'git log --stat --max-count=10'
 alias gd 'git diff'
 alias gt "git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+alias gpull 'git pull --rebase'
 
 function git-search
     git log -S"$argv" --pretty=format:%H | map git show
