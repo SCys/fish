2a8952a0f87e2c54c37b8efb2300c21411eca64a
SCys <me@iscys.com>
2021-11-02 11:22:03 +0800

    + add fisher plug manager + flutter paths + android paths

---
+++ conf.d/android.fish
@@ -0,0 +1,4 @@
+set ANDROID_SDK_ROOT /opt/android/sdk
+
+set PATH $PATH:$ANDROID_SDK_ROOT/cmdline-tools/latest/bin
+set PATH $PATH:$ANDROID_SDK_ROOT/platform-tools
---
+++ conf.d/flutter.fish
@@ -0,0 +1,5 @@
+
+set PATH $PATH:/opt/flutter/bin
+
+set FLUTTER_STORAGE_BASE_URL https://mirrors.tuna.tsinghua.edu.cn/flutter
+set PUB_HOSTED_URL           https://mirrors.tuna.tsinghua.edu.cn/dart-pub
