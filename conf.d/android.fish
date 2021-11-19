02061c88375c0cb01bf65632d3f58e5351d272b2
yanshen.chen <yanshen.chen@geartech.com>
2021-11-19 10:31:45 +0800

    * update android * update git

--- conf.d/android.fish
+++ conf.d/android.fish
@@ -1,4 +1,5 @@
-set ANDROID_SDK_ROOT /opt/android/sdk
+[ -d /opt/android/sdk ] && set ANDROID_SDK_ROOT /opt/android/sdk
+[ -d /Users/scys/Library/Android/sdk/ ] && set ANDROID_SDK_ROOT /Users/scys/Library/Android/sdk/
 
 set PATH $PATH:$ANDROID_SDK_ROOT/cmdline-tools/latest/bin
 set PATH $PATH:$ANDROID_SDK_ROOT/platform-tools
