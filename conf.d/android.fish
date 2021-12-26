591f0401db15952497fe62d17116d8e887e40bac
SCys <me@iscys.com>
2021-12-26 12:48:13 +0800

    + add yarn config

--- conf.d/android.fish
+++ conf.d/android.fish
@@ -1,6 +1,4 @@
 [ -d /opt/android/sdk ] && set ANDROID_SDK_ROOT /opt/android/sdk
 [ -d /Users/scys/Library/Android/sdk/ ] && set ANDROID_SDK_ROOT /Users/scys/Library/Android/sdk/
-
 [ -d $ANDROID_SDK_ROOT/platform-tools ] && set PATH $PATH $ANDROID_SDK_ROOT/platform-tools
 [ -d $ANDROID_SDK_ROOT/cmdline-tools/latest/bin ] && set PATH $PATH $ANDROID_SDK_ROOT/cmdline-tools/latest/bin
-
