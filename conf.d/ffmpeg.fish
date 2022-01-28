cf34f3edafa56965704bda1dec39ee269788a0a7
SCys <me@iscys.com>
2022-01-28 22:30:49 +0800

    * update flutter + ffmpeg

---
+++ conf.d/ffmpeg.fish
@@ -0,0 +1,2 @@
+alias ff_probe 'ffprobe -hide_banner -i '
+#alias ff_convert "ffmpeg -hide_banner -hwaccel cuda -hwaccel_output_format cuda -threads 4 -i $1 -c:v hevc_nvenc -crf 28 -preset medium -c:a aac -b:a 256k $2"
