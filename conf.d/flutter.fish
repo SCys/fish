5d16df78ab979a57ef770563710ff78dcaa86639
SCys <me@iscys.com>
2022-01-28 22:31:12 +0800

    * update flutter config

--- conf.d/flutter.fish
+++ conf.d/flutter.fish
@@ -1,4 +1,4 @@
 [ -d /opt/flutter/bin ] && set PATH $PATH /opt/flutter/bin
 
-set FLUTTER_STORAGE_BASE_URL https://mirrors.tuna.tsinghua.edu.cn/flutter
-set PUB_HOSTED_URL           https://mirrors.tuna.tsinghua.edu.cn/dart-pub
+export FLUTTER_STORAGE_BASE_URL=https://mirrors.tuna.tsinghua.edu.cn/flutter
+export PUB_HOSTED_URL=https://mirrors.tuna.tsinghua.edu.cn/dart-pub
