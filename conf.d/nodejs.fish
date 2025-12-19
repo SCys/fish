41c4a8f8ba92f187015356ab8dae898dff521c66
SCys <me@iscys.com>
2025-12-19 16:21:41 +0800

    * update nvm PATH

--- conf.d/nodejs.fish
+++ conf.d/nodejs.fish
@@ -1,7 +1,7 @@
 [ -d $HOME/.npm-packages/bin ] && set PATH $PATH $HOME/.npm-packages/bin
 
 # nvm
-if [ -s "$NVM_DIR/nvm.sh" ];
+if [ -s "$HOME/.nvm/nvm.sh" ];
         export NVM_DIR="$HOME/.nvm"
         export NVM_NODEJS_ORG_MIRROR=https://mirrors.tuna.tsinghua.edu.cn/nodejs-release/
         . "$NVM_DIR/nvm.sh"  # This loads nvm
