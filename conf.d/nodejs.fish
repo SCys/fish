fef287a37b68ab74885568980a6e34c44856bd83
SCys <me@iscys.com>
2026-01-08 11:52:24 +0800

    Refactor nodejs.fish and config.fish: remove nvm.fish installation check and streamline nvm usage

--- conf.d/nodejs.fish
+++ conf.d/nodejs.fish
@@ -1,12 +1,11 @@
 [ -d $HOME/.npm-packages/bin ] && set PATH $PATH $HOME/.npm-packages/bin
 
-# 检查并安装 nvm.fish 插件，同时设置清华镜像
-if not contains jorgebucaran/nvm.fish (fisher list 2>/dev/null)
-    echo "nvm.fish 未安装，正在通过 fisher 安装..."
-    fisher install jorgebucaran/nvm.fish
-end
 
 if test in_cn
     set -U nvm_mirror https://mirrors.ustc.edu.cn/node/
     #export NVM_NODEJS_ORG_MIRROR=https://mirrors.tuna.tsinghua.edu.cn/nodejs-release/
+end
+
+function nvm
+    bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
 end
--- config.fish
+++ config.fish
@@ -9,16 +9,10 @@ if status is-interactive
   if test $__check_location = "CN"
     set -U in_cn true
   end
-
-  # 检查并安装 nvm.fish 插件，同时设置清华镜像
-  if not contains jorgebucaran/nvm.fish (fisher list 2>/dev/null)
-      echo "nvm.fish 未安装，正在通过 fisher 安装..."
-      fisher install jorgebucaran/nvm.fish
-  end
 end
 
 # 实际运行环境
-if contains jorgebucaran/nvm.fish (fisher list 2>/dev/null) && test -f $HOME/.nvmrc
+if contains edc/bass (fisher list 2>/dev/null) && test -f $HOME/.nvmrc
   # check version from .nvmrc eq current version
   set nvm_current_version (nvm current)
   set nvm_desired_version (cat $HOME/.nvmrc | string trim)
