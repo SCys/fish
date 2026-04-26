00e1c9ee2c968d8a13d185fe7e5035a7466b25be
SCys <me@iscys.com>
2026-04-26 10:00:58 +0800

    * fix test

--- conf.d/flutter.fish
+++ conf.d/flutter.fish
@@ -1,6 +1,6 @@
 [ -d /opt/flutter/bin ] && set PATH $PATH /opt/flutter/bin
 
-if test $in_cn -eq 1
+if $in_cn -eq 1
     set -U FLUTTER_STORAGE_BASE_URL https://mirrors.tuna.tsinghua.edu.cn/flutter
     set -U PUB_HOSTED_URL https://mirrors.tuna.tsinghua.edu.cn/dart-pub
 end
